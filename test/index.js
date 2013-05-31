9a8bccae6afebba6381141ff91535da0d3d33993
Rich Manalang <rmanalang@atlassian.com>
2013-05-31 12:16:27 -0700

    More tests

--- test/index.js
+++ test/index.js
@@ -3,4 +3,5 @@ process.env.FEEBS_OPTS = 'test nooauth';
 
 require('./config_test.js');
 require('./descriptor_test.js');
+require('./oauth_test.js');
 require('./registration_test.js');
---
+++ test/oauth_test.js
@@ -0,0 +1,39 @@
+var helper = require('./test_helper');
+var assert = require('assert');
+var http = require('http');
+var express = require('express');
+var app = express();
+var feebs = require('../index');
+var request = require('request')
+var sinon = require('sinon');
+var addon = {};
+
+describe('OAuth', function(){
+  var server = {};
+
+  before(function(done){
+    app.set('env','development');
+    app.use(express.bodyParser());
+    addon = feebs(app, {});
+
+    server = http.createServer(app).listen(3001, function(){
+      addon.register();
+      done();
+    });
+  });
+
+  after(function(done){
+    server.close();
+    done();
+  });
+
+  it('should be triggered when middleware is activated', function(done){
+    var spy = sinon.spy();
+    addon.on('oauth_verification_triggered', spy);
+    addon.authenticate();
+    assert(spy.called);
+    done();
+  });
+
+
+});
