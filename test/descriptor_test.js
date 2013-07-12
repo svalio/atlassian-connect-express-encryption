51f79664db18f48008b6b338b162bdd688e98072
Robert Bergman <robert.bergman@gmail.com>
2013-07-12 15:44:31 +1000

    Issue #4 - Switch from xml2json to xml2js

--- test/descriptor_test.js
+++ test/descriptor_test.js
@@ -39,19 +39,46 @@ describe('Descriptor', function(){
   });
 
   it('should have variables replaced from the addon config', function(done){
-    assert.equal(addon.descriptor.key(), 'my-test-app-key');
-    assert.equal(addon.descriptor.name(), 'My Test App Name');
-    assert.equal(addon.descriptor.description(), 'My test app description.');
-    assert.equal(addon.descriptor.version(), '1');
-    assert.equal(addon.descriptor.vendorName(), 'My Company');
-    assert.equal(addon.descriptor.vendorUrl(), 'http://example.com');
-    assert.deepEqual(addon.descriptor.permissions(), ['create_oauth_link']);
+    var key = addon.descriptor.key();
+    assert.equal(typeof key, 'string');
+    assert.equal(key, 'my-test-app-key');
+    var name = addon.descriptor.name();
+    assert.equal(typeof name, 'string');
+    assert.equal(name, 'My Test App Name');
+    var description = addon.descriptor.description();
+    assert.equal(typeof description, 'string');
+    assert.equal(description, 'My test app description.');
+    var version = addon.descriptor.version();
+    assert.equal(typeof version, 'string');
+    assert.equal(version, '1');
+    var vendorName = addon.descriptor.vendorName();
+    assert.equal(typeof vendorName, 'string');
+    assert.equal(vendorName, 'My Company');
+    var vendorUrl = addon.descriptor.vendorUrl();
+    assert.equal(typeof vendorUrl, 'string');
+    assert.equal(vendorUrl, 'http://example.com');
+    var permissions = addon.descriptor.permissions();
+    assert.deepEqual(permissions, ['create_oauth_link']);
     var docUrl = addon.descriptor.documentationUrl();
     assert.equal(typeof docUrl, 'string');
     assert.equal(docUrl, 'http://example.com');
     var configUrl = addon.descriptor.configureUrl();
     assert.equal(typeof configUrl, 'string');
     assert.equal(configUrl, '/plugins/servlet/remotable-plugins/my-test-app-key/config-page');
+    done();
+  });
+
+  it('should list webhooks', function(done){
+    var webhooks = addon.descriptor.webhooks();
+    assert.equal(webhooks.length, 1);
+    webhooks = addon.descriptor.webhooks('foo');
+    assert.equal(webhooks.length, 0);
+    webhooks = addon.descriptor.webhooks('remote_plugin_enabled');
+    assert.equal(webhooks.length, 1);
+    var webhook = webhooks[0];
+    assert.equal(webhook.key, 'enabled');
+    assert.equal(webhook.event, 'remote_plugin_enabled');
+    assert.equal(webhook.url, '/enabled');
     done();
   });
 
