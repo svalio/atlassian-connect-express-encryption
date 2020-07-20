0cb8200b604f279da7f364c601777008c4429326
John Jiang <johnjiang101@gmail.com>
2020-07-20 00:30:21 -0700

    Added authentication_test.js

---
+++ test/authentication_test.js
@@ -0,0 +1,8 @@
+const assert = require("assert");
+const { getVerifiedClaims } = require("../lib/middleware/authentication");
+
+describe("authentication", function() {
+  it("exports getVerifiedClaims for apps that need the claims manually", function() {
+    assert(getVerifiedClaims);
+  });
+});
