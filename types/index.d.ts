1b4e959d101632ec470a08811ed43631d7577e7e
Andrew Zhao <azhao@atlassian.com>
2020-12-16 18:11:51 +1100

    ACEJS-147 Added typings

---
+++ types/index.d.ts
@@ -0,0 +1,162 @@
+import * as express from 'express';
+import { EventEmitter } from 'events';
+import { FSWatcher } from 'fs';
+import {Sequelize} from 'sequelize';
+
+// TODO comments
+// TODO replace any with types
+
+type Descriptor = {
+    key: string;
+    name: string;
+    description: string;
+    vendor: {
+        name: string;
+        url: string;
+    };
+    baseUrl: string;
+    links: {
+        self: string;
+        homepage: string;
+    };
+    authentication: {
+        type: string;
+    };
+    scopes: string[];
+}
+
+type Options = {
+    config: {
+        descriptorTransformer: (descriptor: Partial<Descriptor>, config: Config) => Descriptor
+    }
+}
+
+interface ConfigOptions {
+    environment: string;
+    port: string;
+    store: {
+        adapter: string,
+        type: string
+    };
+    expressErrorHandling: boolean;
+    errorTemplate: boolean;
+    validateDescriptor: boolean;
+    localBaseUrl: string;
+    jwt: {
+        validityInMinutes: number;
+    };
+    product: string;
+    hosts: string[];
+    maxTokenAge: number;
+    userAgent: string;
+}
+
+interface Config{
+    port(): string
+    environment(): string;
+    store(): {
+        adapter: string,
+        type: string
+    };
+    expressErrorHandling(): boolean;
+    errorTemplate(): boolean;
+    validateDescriptor(): boolean;
+    localBaseUrl(): string;
+    jwt(): {
+        validityInMinutes: number;
+    };
+    product(): string;
+    hosts(): string[];
+    maxTokenAge(): number;
+    userAgent(): string;
+}
+
+interface StoreAdapter{
+    del(key: string, clientKey: string): Promise<any>
+    get(key: string, clientKey: string): Promise<any>
+    set(key: string, clientKey: string): Promise<any>
+    schema?: Sequelize
+    settings: {
+        dialect: string;
+        logging: boolean
+        storage: string
+    } | 
+    {
+        connectionUrl: string;
+        collectionName: string;
+        databaseName?: string;
+        mongoDbOpts: {
+            retryWrites: boolean;
+            useNewUrlParser: boolean;
+            promiseLibrary?: PromiseConstructor
+        }
+        //opts?!
+    }
+}
+
+type MiddlewareParameters = (request: express.Request, response: express.Response, next: express.NextFunction) => void;
+
+declare const DESCRIPTOR_FILENAME = "atlassian-connect.json";
+
+declare interface Store {
+    register(adapter: any, factory: any): void;
+}
+
+declare class AddOn extends EventEmitter {
+    constructor(app: express.Application, opts?: Options, logger?: Console, callback?: () => void);
+    constructor(app: express.Application);
+    
+
+    middleware(): MiddlewareParameters;
+    authenticate(skipQshVerification: boolean): MiddlewareParameters;
+    loadClientInfo(clientKey: string): Promise<any>; // TODO what's the structure of clientInfo
+    checkValidToken(): MiddlewareParameters | boolean;
+
+    register() : Promise<any>;
+    key: string;
+    name: string;
+    config: Config;
+
+    app: express.Application;  
+
+    deregister(): Promise<void>;
+
+    descriptor: Descriptor;
+
+    schema: Sequelize;
+    settings: StoreAdapter;
+
+    shouldDeregister(): boolean;
+    shouldRegister(): boolean;
+
+    validateDescriptor(): {
+        type: string;
+        message: string;
+        validationResults: {
+            module: string;
+            description: string;
+            value?: unknown;
+            validValues?: string[];
+        }[]
+    }[];
+
+    watcher: FSWatcher;
+
+    /** 
+     * Reloads AddOn descriptor file
+    */
+    reloadDescriptor(): void;
+
+    /**
+     * @param reqOrOpts either an expressRequest object or options
+     * @returns HostClient a httpClient
+     */
+    httpClient(reqOrOpts: { clientKey: string, useAccountId: string }): any;
+    httpClient(reqOrOpts: Request): any;
+}
+type Opts = {config: ConfigOptions}
+
+type AddOnFactory = (app: express.Application, opts?: Opts, logger?: Console, callback?: () => void) => AddOn;
+
+declare const addOnFactory: AddOnFactory;
+export default addOnFactory;
